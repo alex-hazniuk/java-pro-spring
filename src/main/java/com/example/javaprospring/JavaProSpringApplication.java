d9cae6f946df4c6a7ae57010046be049cc1daac6
Alex Hazniuk <ohazniuk@gmail.com>
2023-06-15 11:34:42 +0300

    first commit

---
+++ src/main/java/com/example/javaprospring/JavaProSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaprospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaprospring/JavaProSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaprospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
