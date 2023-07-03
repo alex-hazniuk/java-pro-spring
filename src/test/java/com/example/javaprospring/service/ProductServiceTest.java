56f7fa1c30f7021ba9f4937a54445876543f56c1
Alex Hazniuk <ohazniuk@gmail.com>
2023-07-03 23:31:47 +0300

    implemented task 'Junit/Mokito'

---
+++ src/test/java/com/example/javaprospring/service/ProductServiceTest.java
@@ -0,0 +1,33 @@
+package com.example.javaprospring.service;
+
+import com.example.javaprospring.model.Product;
+import com.example.javaprospring.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+    private static ProductRepository productRepository;
+    private static ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+        productRepository = Mockito.mock(ProductRepository.class);
+        productService = new ProductService(productRepository);
+    }
+
+    @Test
+    void save_ok() {
+        Product product = Product.builder()
+                .id(1)
+                .name("bag")
+                .cost(80)
+                .orderId(2)
+                .build();
+        Mockito.when(productRepository.save(product)).thenReturn(product);
+        Product savedProduct = productService.save(product);
+        assertEquals(product, savedProduct);
+    }
+}
